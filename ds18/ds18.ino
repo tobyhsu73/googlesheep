6a67358abed78ae5fce71afef2fcd84d991d0ac2
tobyhsu73 <73582590+tobyhsu73@users.noreply.github.com>
2021-02-25 11:13:41 +0800

    Create ds18.ino

---
+++ ds18/ds18.ino
@@ -0,0 +1,37 @@
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Arduino數位腳位2接到1-Wire裝置
+#define ONE_WIRE_BUS 2
+
+// 運用程式庫建立物件
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  Serial.begin(115200);
+  
+  // 初始化
+  sensors.begin();
+}
+
+void loop(void)
+{
+  // 要求匯流排上的所有感測器進行溫度轉換（不過我只有一個）
+
+  sensors.requestTemperatures();
+  
+
+  // 取得溫度讀數（攝氏）並輸出，
+  // 參數0代表匯流排上第0個1-Wire裝置
+  //Serial.print("0");
+  Serial.print(sensors.getTempCByIndex(0));
+  //Serial.print("1");
+  Serial.print(sensors.getTempCByIndex(1));
+  //Serial.print("2");
+  Serial.print(sensors.getTempCByIndex(2));
+
+  delay(1000);
+}
